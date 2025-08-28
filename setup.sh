786d309c14e577dc88515ae7157831e7d5cafa05
Animesh Samantaray <animeshsamantaray2@gmail.com>
2025-08-28 16:31:37 +0530

    Ready for Render deployment

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
-mkdir -p ~/.streamlit
+mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-port = \$PORT\n\
-enableCORS = false\n\
 headless = true\n\
-\n\
+enableCORS = false\n\
+port = \$PORT\n\
 " > ~/.streamlit/config.toml
